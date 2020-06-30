2acebfa3b486eb269f4b21e6a30a04faa1f18136
Juan Manuel <livonjm@gmail.com>
2020-06-30 11:27:15 -0300

    correcciones en notificaciones

--- api/notifications/get/index.php
+++ api/notifications/get/index.php
@@ -11,13 +11,13 @@ global $access_token,$collector_id,$notificationJSON;
 
 $lastResource = file_get_contents('../notifications.txt');
 
-$url= $lastResource[0];
+$url= $lastResource;
 
 // REVISAR AQUÍ:
 // Agrega la $url necesaria para revisar el estado del pago en base al recurso recibido de la notificación
 
 // Sustituye el método por su correspondiente: get, put, post, delete
 
-curl_call("GET",."$url","");
+curl_call("GET","$url?access_token=$access_token","");
 
  ?>
